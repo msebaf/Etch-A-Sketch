de7a154ff18cadb1316e5d49f7352f371cf91967
msebaf <msebaf@hotmail.com>
2021-06-22 01:01:44 -0300

    created grid, add paint functionality on hover, pure Js no CSS so far

--- jscript.js
+++ jscript.js
@@ -1,15 +1,52 @@
+
 const container = document.querySelector("#mainContainer");
- function  bla(){
-    console.log ("loaded");
-    for (let i=1; i<=16*16; i++){
-        
+ container.style.display = "inline-flex"
+ 
+ 
+ 
+ function  fillGrid(nombreLinea){
+  
+
+    for (let i=1; i<=16; i++){
         const div = document.createElement("div");
+        div.setAttribute("class", "square");
+        div.addEventListener("mouseover", function(e){
+            div.style.backgroundColor = "red";
+        })
         div.style.width = "30px"
         div.style.height = "30px"
         div.style.border = "4px solid black"
         div.style.backgroundColor = "blue"
-        container.appendChild(div);
+        nombreLinea.appendChild(div);
+        
+        
+        
         
 
     }
+    
 }
+
+function createGrid() {
+    for (let i=1; i<=16; i++){
+        const newLine= document.createElement("div");
+        container.appendChild(newLine)
+        fillGrid(newLine);
+        
+        
+
+    }
+    
+}
+
+
+
+
+
+    
+
+
+
+
+
+
