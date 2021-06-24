b87239b6604db65a6c28456147fc8becedeafc79
msebaf <msebaf@hotmail.com>
2021-06-23 22:24:43 -0300

    added clear grid button

--- jscript.js
+++ jscript.js
@@ -1,6 +1,12 @@
 
+
+const mainMaincontainer = document.querySelector("#mainMainContainer")
 const container = document.querySelector("#mainContainer");
- container.style.display = "inline-flex"
+ container.style.display = "flex"
+ 
+const clearButon = document.querySelector("#clear").addEventListener("click", function(e){
+    makeBlueAgain();
+})
  
  
  
@@ -9,37 +15,37 @@ const container = document.querySelector("#mainContainer");
 
     for (let i=1; i<=16; i++){
         const div = document.createElement("div");
-        div.setAttribute("class", "square");
+        div.setAttribute("class", "squareBlue");
         div.addEventListener("mouseover", function(e){
-            div.style.backgroundColor = "red";
+        div.setAttribute("class", "squareRed")
         })
-        div.style.width = "30px"
-        div.style.height = "30px"
-        div.style.border = "4px solid black"
-        div.style.backgroundColor = "blue"
+        
         nombreLinea.appendChild(div);
         
-        
-        
-        
-
     }
     
 }
 
 function createGrid() {
+    
+
+   
     for (let i=1; i<=16; i++){
         const newLine= document.createElement("div");
         container.appendChild(newLine)
         fillGrid(newLine);
-        
-        
-
+      
     }
     
 }
 
-
+function makeBlueAgain(){
+    const reded = container.querySelectorAll(".squareRed")
+        for (let index = 0; index < reded.length; index++) {
+        const element = reded[index];
+        element.setAttribute("class", "squareBlue")
+    }
+}
 
 
 
